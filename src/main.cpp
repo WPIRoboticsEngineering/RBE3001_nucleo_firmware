ee240e68c1dc941dd6a1483e912b2e26ceb04f3a
diggerdata <robotincad@gmail.com>
2017-06-12 16:06:33 -0400

    Added Servo library

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,36 @@
  * Purple:  SCK  (SPI clock)
  */
 
-#include "AS5050.h"
-#include "mbed.h"
+ #include "AS5050.h"
+ #include "Servo.h"
+ #include "mbed.h"
 
-AS5050 enc1(PB_5, PB_4, PB_3, PA_4); // mosi, miso, sclk, cs
+ // AS5050 enc1(PB_5, PB_4, PB_3, PA_4); // mosi, miso, sclk, cs
+   Servo myservo(PC_9, 5);
 
-int main() {
-  while(1){
-    printf("Angle = %X\n", enc1.angleDegrees());
-    wait_ms(10);
-  }
-}
+ int main() {
+  //  int loop = 0;
+  //  float angle = 0.0;
+  //  while(1){
+  //    angle = enc1.angleRad();
+  //    wait_ms(1);
+  //    loop++;
+   //
+  //    if(loop >= 100) {
+  //      printf("Angle = %4.2f\n", angle);
+  //      loop = 0;
+  //    }
+  //  }
+
+    while(1) {
+        for(int i=0; i<100; i++) {
+          myservo.write(i/100.0);
+            wait(0.01);
+        }
+        for(int i=100; i>0; i--) {
+            myservo.write(i/100.0);
+            wait(0.01);
+        }
+      }
+
+ }
