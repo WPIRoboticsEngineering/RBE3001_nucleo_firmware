a5dfa435a17e723075a2843cb65f622b0ca1ec57
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-19 17:20:12 -0400

    Adding the PID controller template

---
+++ src/MyPid.cpp
@@ -0,0 +1,28 @@
+
+#include "MyPid.h"
+
+PIDimp::PIDimp(){}
+float PIDimp::getPosition( ){
+  return 0;
+}
+void PIDimp::setOutputLocal( float){
+
+}
+int PIDimp::resetPosition( int){
+  return 0;
+}
+void PIDimp::onPidConfigureLocal(){
+
+}
+
+void PIDimp::MathCalculationPosition( float currentTime){
+   MathCalculationPositionDefault( currentTime);
+}
+void PIDimp::MathCalculationVelocity( float currentTime){
+   MathCalculationVelocityDefault( currentTime);
+}
+
+PidLimitEvent* PIDimp::checkPIDLimitEvents(){
+
+  return &currentEvent;
+}
---
+++ src/MyPid.h
@@ -0,0 +1,16 @@
+#ifndef MYPID_BOWLER_H
+#define MYPID_BOWLER_H
+#include <PID_Bowler.h>
+class PIDimp : public PIDBowler{
+public:
+  PIDimp();
+  float getPosition();
+  void setOutputLocal( float);
+  int resetPosition( int);
+  void onPidConfigureLocal();
+  void MathCalculationPosition( float);
+  void MathCalculationVelocity( float);
+  PidLimitEvent* checkPIDLimitEvents();
+
+};
+#endif
