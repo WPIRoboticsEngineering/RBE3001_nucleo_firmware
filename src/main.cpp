826ff904773dd0272bfcb25700f55a452a100b19
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-20 12:26:02 -0400

    Adding the multi case for PID

--- src/main.cpp
+++ src/main.cpp
@@ -12,45 +12,32 @@
  * Purple:  SCK  (SPI clock)
  */
 
- #include "AS5050.h"
- #include "Servo.h"
- #include "mbed.h"
- #include "MyPid.h"
-PIDimp * testPid;
+#include "AS5050.h"
+#include "Servo.h"
+#include "mbed.h"
+#include "MyPid.h"
+#define  numberOfPid  3
+
+static PIDimp*  pid[numberOfPid];
 double kp=1;
 double ki=0;
 double kd=0;
+
  int main() {
-   testPid = new PIDimp( new Servo(PC_9, 5),
-                         new AS5050(PB_5, PB_4, PB_3, PA_4));
-   testPid->setPIDConstants(kp,ki,kd);
-   testPid->InitilizePidController();
-
-
-  //  int loop = 0;
-  //  float angle = 0.0;
-  //  while(1){
-  //    angle = enc1.angleRad();
-  //    wait_ms(1);
-  //    loop++;
-   //
-  //    if(loop >= 100) {
-  //      printf("Angle = %4.2f\n", angle);
-  //      loop = 0;
-  //    }
-  //  }
+   for (int i=0;i<numberOfPid;i++){
+     pid[i] = new PIDimp( new Servo(PC_9, 5),
+                           new AS5050(PB_5, PB_4, PB_3, PA_4));
+     pid[i]->setPIDConstants(kp,ki,kd);
+     pid[i]->InitilizePidController();
+   }
 
     while(1) {
-      testPid->updatePosition();
-      testPid->updateControl();
-        // for(int i=0; i<100; i++) {
-        //   myservo.write(i/100.0);
-        //     wait(0.01);
-        // }
-        // for(int i=100; i>0; i--) {
-        //     myservo.write(i/100.0);
-        //     wait(0.01);
-        // }
-      }
+      // update all positions fast and together
+      for (int i=0;i<numberOfPid;i++)
+        pid[i]->updatePosition();
+      // next update all control outputs
+      for (int i=0;i<numberOfPid;i++)
+        pid[i]->updateControl();
 
+    }
  }
