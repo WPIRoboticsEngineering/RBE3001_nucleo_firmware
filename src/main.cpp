440b5da0febd0b6382c9f44f01cd3cfee704c059
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-20 13:29:14 -0400

    Adding the example functions to run the PID calibrations

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,21 @@ double kd=0;
                          new AS5050(PB_5, PB_4, PB_3, PA_3));
    pid[2] = new PIDimp( new Servo(PC_11, 5),
                          new AS5050(PB_5, PB_4, PB_3, PA_2));
-   pid[0]->stat.config.Polarity
+   // Invert the direction of the motor vs the input
+   //pid[0]->state.config.Polarity = true;
+
    for (int i=0;i<numberOfPid;i++){
      pid[i]->setPIDConstants(kp,ki,kd);
      pid[i]->InitilizePidController();
    }
-
+   // Run PID controller calibration
+   //pid[0]->runPidHysterisisCalibration();
+   // Run a homing procedure down
+   //pid[0]->startHomingLink( CALIBRARTION_home_down, 123);
+   // Run a homing procedure up
+   //pid[0]->startHomingLink( CALIBRARTION_home_up, 123);
+   // Run a homing procedure to scale the velocity outputs
+   //pid[0]->startHomingLink( CALIBRARTION_home_velocity, 123);
     while(1) {
       // update all positions fast and together
       for (int i=0;i<numberOfPid;i++)
