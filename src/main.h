1c7e80a67149c5392c883f52c8119d793b79b7e5
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-21 21:38:30 -0400

    PID controller is tested working on hardware (#2)
    * Added blink code and sample SPI
    
    * Delete AS5050A.cpp
    
    * Added a main.cpp
    
    * Issue #1
    
    * INTEGRATED AS5050 SUBMODULE
    
    Removed gitignore for /lib folder. Updated AS5050 library to latest.
    
    * #1 - Added test code for AS5050 library#1
    
    * Added Servo library
    
    * Adding bowler com
    
    * Adding modules
    
    * Update .gitmodules
    
    * updating library
    
    * Removing bowlercom
    
    * removing bowler com
    
    * =PID controller template
    
    * Adding the PID controller template
    
    * =Updating bowler
    
    * =Adding the PID interface
    
    * Updating the PID interface now compiles
    
    * Updating PID library
    
    * Adding clock and PID example
    
    * Adding the multi case for PID
    
    * Updating PID
    
    * Adding more explicate examples
    
    * Adding the delay for the control loop
    
    * moving PID development to WPI repo
    
    * Adding polarity swap example
    
    * Updating the header
    
    * Adding the example functions to run the PID calibrations
    
    * Added ports to main.h
    
    * Fixed SPI pins
    
    * Adding prints for controller state
    
    * Slowing loop and adding control back in
    
    * Updating the PID controller
    
    * Using timer for encoders to prevend timer errors causing bad data
    
    * Disable PID while encoders are warming up
    
    * Updaitng the PID controller to work with linear interpolation
    
    * Adding timing offsets for the beginning of read on the encoders. This prevents a false first read to be loaded in the reset and causing the default to be unreasonable.
    
    * Using IRQ blocking to prevent currupt data between timer and main, this should be switched to a Thread/RtosTimer combo
    
    * Updating encoder library
    
    * Updating encoder library
    
    * Handle encoder errors in timer thread
    
    * Adding the configurations for bounding to the PID controller to manage
    
    * Updating PID library
    
    * Updating encoder library
    
    * Updating PID library
    
    * Valid configurations for the servo arm motors
    
    * Updating PID library
    
    * Adding comments
    
    * Adding comments
    
    * Adding comments
    
    * Adding comments and cleanup

---
+++ src/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "AS5050.h"
+#include "mbed.h"
+#include "Clock.h"
+
+#define SERVO_1 PC_6
+#define SERVO_2 PB_15
+#define SERVO_3 PB_13
+
+#define MOSI PB_5
+#define CLK PB_3
+#define MISO PB_4
+
+#define ENC_1 PD_14
+#define ENC_2 PD_15
+#define ENC_3 PF_12
+
+#endif
