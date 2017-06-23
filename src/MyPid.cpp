01cb2a94d9a2717f3d56491a422c6e1f2a55c2c2
Kevin Harrington <mad.hephaestus@gmail.com>
2017-06-23 15:27:20 -0400

    Full configuration of the PD velocity controller

--- src/MyPid.cpp
+++ src/MyPid.cpp
@@ -19,13 +19,16 @@ float PIDimp::resetPosition( float newCenter){
 }
 void PIDimp::onPidConfigureLocal(){
   setPIDConstants(kp,ki,kd);
+  // pd velocity constants
+  state.config.V.P=vkp;
+  state.config.V.D=vkd;
   // this will change the sign of the output signal, and will flip between converging and and diverging
   state.config.Polarity=true;
   state.config.stop=0.5f;// the center value for the servo object
   // this is the maximum value that should come in through setOutputLocal
   state.config.outputMaximum=0.7f;
   // this is the minimum value that should come in through setOutputLocal
-  state.config.outputMinimum=0.4f;
+  state.config.outputMinimum=0.3f;
   // the smallest increment of change for the output
   state.config.outputIncrement=0.0005f;
   // the upper and lower hystersis values for where the motor starts moving
