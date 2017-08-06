9ccffd1a43b805423549f668c94341cda1d97a58
Kevin Harrington <mad.hephaestus@gmail.com>
2017-08-06 15:27:08 -0400

    Clearing packets and cleaner Dummy PID

--- src/drivers/DummyPID.cpp
+++ src/drivers/DummyPID.cpp
@@ -5,8 +5,8 @@ DummyPID::DummyPID(){
 
 }
 // Return the current position of the system
-float DummyPID::getPosition( ){
-  return state.interpolate.set+1;
+float DummyPID::getPosition(){
+  return state.interpolate.set;
 }
 //Send controller signel to the motors, bounded and scaled by the configurations
 void DummyPID::setOutputLocal( float currentOutputValue){}
@@ -14,11 +14,31 @@ float DummyPID::resetPosition( float newCenter){
   // optionally reset the encoder object
   return getPosition();
 }
-void DummyPID::onPidConfigureLocal(){}
+void DummyPID::onPidConfigureLocal(){
+  setPIDConstants(kp,ki,kd);
+  // pd velocity constants
+  state.config.V.P=vkp;
+  state.config.V.D=vkd;
+  // this will change the sign of the output signal, and will flip between converging and and diverging
+  state.config.Polarity=true;
+  state.config.stop=0.5f;// the center value for the servo object
+  // this is the maximum value that should come in through setOutputLocal
+  state.config.outputMaximum=0.7f;
+  // this is the minimum value that should come in through setOutputLocal
+  state.config.outputMinimum=0.3f;
+  // the smallest increment of change for the output
+  state.config.outputIncrement=0.0005f;
+  // the upper and lower hystersis values for where the motor starts moving
+  state.config.upperHistoresis = state.config.stop+0.01;
+  state.config.lowerHistoresis = state.config.stop-0.01;
+  // a value in encoder units that representst the noise floor of the sensor when detecting stall homing
+  state.homing.homingStallBound = 20.0f;
+  printf("\nPID initialized");
+}
 
-void DummyPID::MathCalculationPosition( float currentTime){}
+void DummyPID::MathCalculationPosition( float currentTime){RunAbstractPIDCalc( currentTime);}
 
-void DummyPID::MathCalculationVelocity( float currentTime){}
+void DummyPID::MathCalculationVelocity( float currentTime){RunPDVel();}
 // User provided the events tat constitute stall or error conditions.
 PidLimitEvent* DummyPID::checkPIDLimitEvents(){
   currentEvent.type=NO_LIMIT;
