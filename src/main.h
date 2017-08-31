e634e45b8c04d03332dad4613d2f4350d99f6866
Kevin Harrington <mad.hephaestus@gmail.com>
2017-08-31 10:01:37 -0400

    Adding the HDMI documentation

--- src/main.h
+++ src/main.h
@@ -12,23 +12,28 @@
 #include "main.h"
 #include "USBHID.h"
 #include "RunEvery.h"
+//GROUND     HDMI 19,9,8,7
+//3.3 volts  HDMI 18
+//Motor 8.4v HDMI 6,5,4 
 
 //Coms
 #include "coms/PidServer.h"
+// HDMI 1
 #define SERVO_1 PE_9
 #define SERVO_2 PE_11
 #define SERVO_3 PE_13
 
-#define MOSI PB_5
-#define CLK PB_3
-#define MISO PB_4
-//Full turn BCB values
+#define MOSI PB_5 // HDMI 16
+#define CLK PB_3 // HDMI 13
+#define MISO PB_4 // HDMI 15
+//Full turn PCB values
+//HDMI 17
 #define ENC_1 PC_8
 #define ENC_2 PC_9
 #define ENC_3 PC_10
-
+//HDMI 12
 #define LOAD_1 PA_3
 #define LOAD_2 PC_0
-#define LOAD_1 PC_3
+#define LOAD_3 PC_3
 
 #endif
