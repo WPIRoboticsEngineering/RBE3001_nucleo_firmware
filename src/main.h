f22e0e46660f17c6b444d7bee6b4bfaf8d8b7166
Kevin Harrington <mad.hephaestus@gmail.com>
2017-09-03 17:37:42 -0400

    Attempt to resolve Issue #1
    This commit changes the mbed-os version to a fork with all but the
    nucleo firmware removed. This fork is maintained for the purpose of
    removing the ambiguous pin definitions. The pin names header file is
    also added explicitly.

--- src/main.h
+++ src/main.h
@@ -3,6 +3,9 @@
 
 #include "AS5050.h"
 #include "mbed.h"
+#include "../mbed-os/targets/TARGET_STM/TARGET_STM32F7/TARGET_STM32F746xG/TARGET_NUCLEO_F746ZG/PinNames.h"
+#include "../mbed-os/drivers/AnalogIn.h"
+#include "../mbed-os/drivers/AnalogOut.h"
 #include "drivers/Clock.h"
 #include "Servo.h"
 #include "mbed.h"
@@ -12,12 +15,14 @@
 #include "main.h"
 #include "USBHID.h"
 #include "RunEvery.h"
+//Coms
+#include "coms/PidServer.h"
+
+// DEFINES
+
 //GROUND     HDMI 19,1,2,3
 //3.3 volts  HDMI 18
 //Motor 8.4v HDMI 6,5,4 
-
-//Coms
-#include "coms/PidServer.h"
 // HDMI 9
 #define SERVO_1 PE_9
 #define SERVO_2 PE_11
